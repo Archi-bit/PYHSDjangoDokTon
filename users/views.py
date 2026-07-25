4bf2cea5e32556fc6cb36c0df5becdcd7c6d00e7
Archi-bit <argennnmmmtv@gmail.com>
2026-07-26 05:49:19 +0600

    Templates for users added

---
+++ users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+def login(request):
+    context = {
+        'title': 'Home - Авторизация'
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Home - Регистрация'
+    }
+    return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    context = {
+        'title': 'Home - Личный кабинет'
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    ...
